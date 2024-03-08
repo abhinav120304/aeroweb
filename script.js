7e774ac64a4219a37fc3bee6924ee7546bd54658
abhinav120304 <128918249+abhinav120304@users.noreply.github.com>
2024-03-08 21:26:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+// Image slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("gallery-slide");
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }    
+    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+// Responsive navigation menu
+const toggleButton = document.getElementById('toggleButton');
+const navList = document.getElementById('navList');
+
+toggleButton.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
